e005dad1a3bc45b8fb8e4fa701eb660bd9ea8ec8
Shubhamrmishra <shubhamrmishra999@gmail.com>
2024-02-21 17:15:57 +0530

    second

--- src/main/java/com/employee/controller/EmpController.java
+++ src/main/java/com/employee/controller/EmpController.java
@@ -6,11 +6,13 @@ import com.employee.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+//@Controller
 @RequestMapping("/employees")
 public class EmpController {
 
@@ -33,6 +35,7 @@ public class EmpController {
             return empService.getEmployeeById(id);
         }
 
+        @CrossOrigin("http://localhost:4200/")
         @GetMapping("/")
         public List<EmpDto> getAllEmployees() {
             return empService.getAllEmployees();
@@ -47,6 +50,13 @@ public class EmpController {
         public EmpDto updateEmployee(@PathVariable Integer id,@RequestBody EmpDto empDto){
             return empService.updateEmployeeById(id,empDto);
         }
+
+        @GetMapping("/home")
+        public String getHome(){
+            return "home";
+        }
+
+
 
 }
 
